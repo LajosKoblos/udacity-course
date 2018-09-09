0ef96702ca86980a7c1b31f8b5e4865aee27a30e
Lajos Köblös <lajos@koblos.org>
2018-09-09 22:50:17 +0200

    Solve task 02

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -11,12 +11,21 @@ import 'package:flutter/material.dart';
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  static const _HEIGHT = 100.0;
+  static const _BORDER_RADIUS = 50.0;
+
+  final MaterialColor color;
+  final String name;
+  final IconData iconLocation;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category(this.name, this.color, this.iconLocation)
+  // TODO: why key to super
+  // TODO; examine assert
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +37,35 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Container(
+      height: _HEIGHT,
+      padding: EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          print('I was tapped!');
+        },
+        splashColor: color,
+        highlightColor: color,
+        borderRadius: BorderRadius.all(Radius.circular(_BORDER_RADIUS)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Icon(
+                iconLocation,
+                size: 60.0,
+              ),
+            ),
+            Center(
+              child: Text(
+                name,
+                style: Theme.of(context).textTheme.headline,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

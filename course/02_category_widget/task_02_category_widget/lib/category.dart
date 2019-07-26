ffaf9f1387a04ac9075ef9b0a829e39b8a9c6999
Lajos Koblos <lajos@koblos.org>
2019-07-26 16:27:11 +0200

    Solve task_02_category_widget

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -6,17 +6,31 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _height = 100.0;
+final _borderRadius = _height / 2;
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final IconData categoryIcon;
+  final String categoryName;
+  final MaterialColor categoryColor;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
-  // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category({
+    Key key,
+    @required this.categoryName,
+    @required this.categoryIcon,
+    @required this.categoryColor,
+  })  : assert(categoryName != null),
+        assert(categoryIcon != null),
+        assert(categoryColor != null),
+        super(key: key);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -27,7 +41,35 @@ class Category extends StatelessWidget {
   // Theme ancestor in the tree. Below, we obtain the display1 text theme.
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _height,
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(_borderRadius)),
+          highlightColor: categoryColor,
+          splashColor: categoryColor,
+          onTap: () {
+            print('I was tapped!');
+          },
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Icon(
+                  categoryIcon,
+                  size: 60.0,
+                ),
+              ),
+              Text(
+                categoryName,
+                style: Theme.of(context).textTheme.headline,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

ccc1472af60a1fe0c8cff897ce49332ead3c151f
Lajos Koblos <lajos@koblos.org>
2019-07-26 18:54:09 +0200

    Solve task_03_category_route

--- course/03_category_route/task_03_category_route/lib/main.dart
+++ course/03_category_route/task_03_category_route/lib/main.dart
@@ -4,8 +4,7 @@
 
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-
-// TODO: Import the CategoryRoute widget
+import 'package:task_03_category_route/category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -14,16 +13,15 @@ void main() {
 
 /// This widget is the root of our application.
 ///
-/// The first screen we see is a list [Categories].
+/// The first screen we see is a list [Categories].h
 class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
       // our home should now point to an instance of the CategoryRoute widget.
-      home: Container(),
+      home: CategoryRoute(),
     );
   }
 }
